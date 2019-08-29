9048550016cd7d2f1b338213e3671b12b42dbc6c
Valentino Crespo <valentinocrespo80@gmail.com>
2019-08-28 19:27:06 -0500

    "First tech-jobs mvc commit"

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,4 +24,25 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
-}
+    @RequestMapping(value = "results")
+    public String processSearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        if (searchType.equals("all")){
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("jobs", jobs);
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+                model.addAttribute("columns", ListController.columnChoices);
+                model.addAttribute("jobs", jobs);
+
+            }
+        return "search";
+
+        }
+
+    }
+
+
+
+
